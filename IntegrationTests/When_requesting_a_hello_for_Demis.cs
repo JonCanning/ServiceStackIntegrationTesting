700d65eb5bc20e96d97d09ce94823b80cf0a8805
Jon Canning <jon.canning@gmail.com>
2012-04-16 19:16:59 +0100

    Added json test

--- IntegrationTests/When_requesting_a_hello_for_Demis.cs
+++ IntegrationTests/When_requesting_a_hello_for_Demis.cs
@@ -9,12 +9,18 @@ namespace IntegrationTests
     class When_requesting_a_hello_for_Demis
     {
         const string BasePath = "http://localhost:8888/";
-        readonly JsvServiceClient jsvServiceClient = new JsvServiceClient(BasePath);
+        readonly JsonServiceClient jsonServiceClient = new JsonServiceClient(BasePath);
 
         [Test]
-        public void Then_hello_Demis_should_be_returned()
+        public void Then_result_should_be_Hello_Demis()
         {
-            jsvServiceClient.Get<HelloResponse>(BasePath + "hello/Demis").Result.Should().Be("Hello, Demis");
+            jsonServiceClient.Get<HelloResponse>(BasePath + "hello/Demis").Result.Should().Be("Hello, Demis");
+        }
+
+        [Test]
+        public void Then_json_should_contain_Hello_Demis()
+        {
+            jsonServiceClient.Send<object>(new Hello { Name = "Demis" }).ToString().Should().Contain("{{\"Result\":\"Hello, Demis\"}}");
         }
     }
 }
